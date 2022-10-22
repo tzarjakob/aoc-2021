38f87f1f042960a78661021e7e1c715a76c2a9a0
tzarjakob <giacomo.mazzucchi@protonmail.com>
2022-10-22 22:50:24 +0200

    day 16 first part solved

--- 16/day16/lib/day16.ex
+++ 16/day16/lib/day16.ex
@@ -1,42 +1,45 @@
+defmodule Packet do
+  defstruct ver: 0, val: 0, pl: 0
+end
+
 defmodule Day16 do
   def eval(list, num) do
-    {val, r_list} = Enum.split(list, num)
-    parsed_val = List.to_string(val) |> Integer.parse(2) |> elem(0)
-    {parsed_val, r_list}
+    if Enum.count(list) < num do
+      {:end, []}
+    else
+      {val, r_list} = Enum.split(list, num)
+      parsed_val = List.to_string(val) |> Integer.parse(2) |> elem(0)
+      {parsed_val, r_list}
+    end
   end
 
   def eval_lit_value(list, prec, times) do
     {parsed_val, r_list} = eval(list, 5)
 
     cond do
-      parsed_val < 16 -> {r_list, parsed_val, times + 1}
-      true -> eval_lit_value(r_list, prec * 2 ** 4 + (parsed_val - 16), times + 1)
+      parsed_val < 16 -> {r_list, prec * 16 + parsed_val, times + 1}
+      true -> eval_lit_value(r_list, prec * 16 + (parsed_val - 16), times + 1)
     end
   end
 
-  def parse_subpackets_len(list, sub_len) when sub_len > 10 do
-    # IO.puts(sub_len)
-    {val, c_len, r_list} = parse_packet(list) # |> IO.inspect()
-    # IO.puts("sub_len - c_len = #{sub_len - c_len}")
-    [val] ++ parse_subpackets_len(r_list, sub_len - c_len)
+  def parse_subpackets_len(list, sub_len) do
+    if Enum.all?(list, fn elem -> elem == "0" end) or sub_len == 0 do
+      [] ++ [list]
+    else
+      IO.puts("sublen = #{sub_len}")
+      {packet, r_list} = parse_packet(list) # |> IO.inspect()
+      IO.puts("sublen = #{sub_len} | #{sub_len - packet.pl}")
+      [packet] ++ parse_subpackets_len(r_list, sub_len - packet.pl)
+    end
   end
-
-  def parse_subpackets_len(_list, _sub_len) do
-    []
-  end
-
-  #def parse_subpackets_len(_list, _sub_len) do
-  #  IO.puts("vecio, qua non ce la fa a fare un altro pacchetto")
-  #  exit(122)
-  #end
 
   def parse_n_subpackets(list, sub_num) when sub_num > 0 do
-    {val, _c_len, r_list} = parse_packet(list)
-    [val] ++ parse_n_subpackets(r_list, sub_num - 1)
+    {packet, r_list} = parse_packet(list)
+    [packet] ++ parse_n_subpackets(r_list, sub_num - 1)
   end
 
-  def parse_n_subpackets(_list, sub_num) when sub_num == 0 do
-    []
+  def parse_n_subpackets(list, sub_num) when sub_num == 0 do
+    [] ++ [list]
   end
 
   def parse_subpackets(list, _operator) do
@@ -46,48 +49,48 @@ defmodule Day16 do
       len_id == 0 ->
         # next 15 bits -> subpacket length
         {sub_len, list} = eval(list, 15)
-        parse_subpackets_len(list, sub_len)
+        res = parse_subpackets_len(list, sub_len) |> Enum.reverse()
+        {hd(res), Enum.reverse(tl(res)), 16}
 
       len_id == 1 ->
         # next 11 bits -> subpacket number
         {sub_num, list} = eval(list, 11)
-        parse_n_subpackets(list, sub_num)
+        res = parse_n_subpackets(list, sub_num) |> Enum.reverse()
+        {hd(res), Enum.reverse(tl(res)), 12}
     end
+  end
+
+  def eval_packets(packets, operator, used_bits) when operator == :tumare do
+    version_sum = Enum.map(packets, fn packet -> packet.ver end) |> Enum.sum()
+    packet_len = Enum.map(packets, fn packet -> packet.pl end) |> Enum.sum()
+    %Packet{ver: version_sum, val: -1, pl: packet_len + used_bits}
   end
 
   def parse_packet(list) do
     {version, list} = eval(list, 3)
     {type_id, list} = eval(list, 3)
-    IO.puts("version = #{version}, type_id = #{type_id}")
 
     cond do
       type_id == 4 ->
         {list, lit_value, times} = eval_lit_value(list, 0, 0)
-        {%{version: version, value: lit_value}, 6 + times * 5, list}
+        packet_len = 6 + times * 5
+        {%Packet{ver: version, val: lit_value, pl: packet_len}, list}
 
       true ->
-        subpacks = parse_subpackets(list, :tumare) # |> IO.inspect()
-
-
-        # subpacks_version_sum = subpacks |> Enum.map(fn val -> val.version end) |> Enum.sum()
-        # IO.puts("subpacks_version_sum = #{subpacks_version_sum}")
-        # as value you should return using the specific operator
-        {%{version: 0, value: 1}, 6, list}
+        IO.puts("version = #{version}, type_id = #{type_id}, len = 6")
+        {r_list, subpacks, used_bits} = parse_subpackets(list, :tumare)
+        res = eval_packets([%Packet{ver: version, val: :tumare, pl: 6}] ++ subpacks, :tumare, used_bits)
+        {res, r_list}
     end
   end
 
-  def day16_solution() do
-    # data = File.read!('./input.txt')
-    # data = "8A004A801A8002F478"
-    data = "620080001611562C8802118E34"
-
+  def main(data) do
     input_string =
       elem(Integer.parse(data, 16), 0)
       |> Integer.to_string(2)
       |> String.pad_leading(String.length(data) * 4, "0")
       |> String.graphemes()
 
-    {val, len, list} = parse_packet(input_string)
-    IO.inspect({val, len, list})
+    parse_packet(input_string) |> IO.inspect()
   end
 end
